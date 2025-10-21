1a7305f8bda6f055cac11bdbbda08a13a8cd9657
sakshee-pawar <sakshee@acpce.ac.in>
2025-10-21 15:27:43 +0530

    Delete client directory

--- client/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggles = document.querySelectorAll('.toggle');
-  toggles.forEach(btn => {
-    btn.addEventListener('click', () => {
-      document.body.classList.toggle('sidebar-open');
-    });
-  });
-});
